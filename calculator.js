34bafcb089e40779bb873fad6db8118cc96fc7dc
Klemen <klemen.bussines12@gmail.com>
2025-06-02 22:51:46 +0200

    Feat: displaying clicked value on screen
    Actually I saved first number, operator and
    second number so I will be able to use them later.

--- calculator.js
+++ calculator.js
@@ -3,22 +3,8 @@ function main(){
   secondInputedNumber = ""
   inputedOperator = ""
 
-}
+  displayClickedValue()
 
-function operate(operator, firstNumber, secondNumber){
-  switch(operator){
-    case "+":
-      addNumbers(firstNumber, secondNumber)
-
-    case "-":
-      substractNumbers(firstNumber, secondNumber)
-
-    case "*":
-      multiplyNumbers(firstNumber, secondNumber)
-      
-    case "/":
-      divideNumbers(firstNumber, secondNumber)
-  }
 }
 
 function addNumbers(num1, num2){
@@ -36,5 +22,55 @@ function multiplyNumbers(num1, num2){
 function divideNumbers(num1, num2){
   return num1 / num2
 }
+
+function operate(operator, firstNumber, secondNumber){
+  switch(operator){
+    case "+":
+      addNumbers(firstNumber, secondNumber)
+
+    case "-":
+      substractNumbers(firstNumber, secondNumber)
+
+    case "*":
+      multiplyNumbers(firstNumber, secondNumber)
+
+    case "/":
+      divideNumbers(firstNumber, secondNumber)
+  }
+}
+
+function displayClickedValue(){
+  const digitBtns = document.querySelectorAll(".digit")
+  const calculatorDisplay = document.querySelector(".calculator-display")
+  const operatorBtns = document.querySelectorAll(".operator")
+  let firstInputedDigits = ""
+  let inputedOperator = ""
+  let secondInputedDigits = ""
+
+  operatorBtns.forEach((operator) =>{
+    operator.addEventListener("click", () =>{
+      calculatorDisplay.textContent += operator.textContent
+      inputedOperator += operator.textContent
+    })
+  })
+
+  digitBtns.forEach((digitBtn) =>{
+    digitBtn.addEventListener("click", () =>{
+      calculatorDisplay.textContent += digitBtn.textContent
+      if(inputedOperator === ""){
+        firstInputedDigits += digitBtn.textContent  
+      }else{
+        secondInputedDigits += digitBtn.textContent
+      }
+      
+    })
+  })
+
+  
+
+}
+
+
+
 
 main()
