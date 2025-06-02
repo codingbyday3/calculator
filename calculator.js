a0aa5fe668dfde56bb1ba736285f6bb0991eccf8
Klemen <klemen.bussines12@gmail.com>
2025-06-02 21:35:34 +0200

    Feat: function which use wanted operator

--- calculator.js
+++ calculator.js
@@ -1,3 +1,26 @@
+function main(){
+  firstInputedNum = ""
+  secondInputedNumber = ""
+  inputedOperator = ""
+
+}
+
+function operate(operator, firstNumber, secondNumber){
+  switch(operator){
+    case "+":
+      addNumbers(firstNumber, secondNumber)
+
+    case "-":
+      substractNumbers(firstNumber, secondNumber)
+
+    case "*":
+      multiplyNumbers(firstNumber, secondNumber)
+      
+    case "/":
+      divideNumbers(firstNumber, secondNumber)
+  }
+}
+
 function addNumbers(num1, num2){
   return num1 + num2
 }
@@ -13,3 +36,5 @@ function multiplyNumbers(num1, num2){
 function divideNumbers(num1, num2){
   return num1 / num2
 }
+
+main()
